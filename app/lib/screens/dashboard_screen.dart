d2ec1f229c3afcf030ce92456e664046631ab7d0
Amin <m.aminwhat@gmail.com>
2023-08-23 00:02:47 +0330

    complete window manager and tray

--- app/lib/screens/dashboard_screen.dart
+++ app/lib/screens/dashboard_screen.dart
@@ -1,13 +1,55 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:system_tray/system_tray.dart';
+import 'package:window_manager/window_manager.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  void initState() {
+    if (Platform.isWindows) {
+      WindowManager.instance.setAlignment(Alignment.bottomRight, animate: true);
+      WindowManager.instance.hide();
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Everything is fine',
+              ),
+              Icon(Icons.check_circle_outline_outlined, color: Colors.green),
+            ],
+          ),
+          const SizedBox(height: 35),
+          ElevatedButton(
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Colors.red),
+            ),
+            onPressed: () {},
+            child: const Padding(
+              padding: EdgeInsets.all(25),
+              child: Text(
+                'Danger??!',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
